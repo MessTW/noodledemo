c7b810a2ae71da22a43bebd90998d6265853249b
Leonid <leonidpam1183@mail.ru>
2023-01-25 21:08:48 +0500

    Add files via upload

--- pages/snap/script.js
+++ pages/snap/script.js
@@ -1,25 +1,25 @@
 // translate mouse movement into 3D rotation
-window.addEventListener("mousemove", (e) => {
-	w = window.innerWidth;
-	h = window.innerHeight;
+window.addEventListener("mousemove", e => {
+  w = window.innerWidth;
+  h = window.innerHeight;
 
-	const rotx = 7 - e.pageY * (14 / h);
-	const roty = -8 + e.pageX * (16 / w);
+  const rotx = 7 - e.pageY * (14 / h);
+  const roty = -8 + e.pageX * (16 / w);
 
-	document.getElementById(
-		"card"
-	).style.transform = `rotateY(${roty}deg) rotateX(${rotx}deg)`;
+  document.getElementById(
+  "card").
+  style.transform = `rotateY(${roty}deg) rotateX(${rotx}deg)`;
 });
 
 // translate touch movement into 3D rotation
-window.addEventListener("touchmove", (e) => {
-	w = window.innerWidth;
-	h = window.innerHeight;
+window.addEventListener("touchmove", e => {
+  w = window.innerWidth;
+  h = window.innerHeight;
 
-	const rotx = 7 - e.changedTouches[0].pageY * (14 / h);
-	const roty = -10 + e.changedTouches[0].pageX * (20 / w);
+  const rotx = 7 - e.changedTouches[0].pageY * (14 / h);
+  const roty = -10 + e.changedTouches[0].pageX * (20 / w);
 
-	document.getElementById(
-		"card"
-	).style.transform = `rotateY(${roty}deg) rotateX(${rotx}deg)`;
+  document.getElementById(
+  "card").
+  style.transform = `rotateY(${roty}deg) rotateX(${rotx}deg)`;
 });
